1e1c4989a69bdaf94fc86f5c0b90b4d22103fbfe
princeGedeon <guedjegedeon03@gmail.com>
2022-05-02 16:00:32 +0100

    Fonction de base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from vosk import Model,KaldiRecognizer
 import speech_recognition
+import pyttsx3
 import wave
 import json
 import os
@@ -9,3 +10,27 @@ class VoiceAssitant:
     sexe=""
     speech_language=""
     recognitionlanguage=""
+
+def setup_assistant_voice():
+    voices=ttsEngine.getProperty('voices')[26]
+    if assistant.speech_language=='fr':
+        ttsEngine.setProperty("voice",voices.id)
+
+
+
+if __name__=="__main__":
+    recognizer=speech_recognition.Recognizer()
+    microphone=speech_recognition.Microphone()
+
+    ttsEngine=pyttsx3.init()
+
+    assistant=VoiceAssitant()
+    assistant.name="Prince"
+    assistant.sexe="Male"
+    assistant.speech_language="fr"
+
+    setup_assistant_voice()
+
+    while True:
+        pass
+
