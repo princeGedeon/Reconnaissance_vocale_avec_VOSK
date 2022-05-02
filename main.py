e8cef9ee5b653f46fac990dee0614de815071ea8
princeGedeon <guedjegedeon03@gmail.com>
2022-05-02 17:27:25 +0100

    Correction des bugs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pyaudio
+import vosk
 from vosk import Model,KaldiRecognizer
 import speech_recognition
 import pyttsx3
@@ -11,11 +13,33 @@ class VoiceAssitant:
     speech_language=""
     recognitionlanguage=""
 
+
+def recognize(model: vosk.Model, phrase_wav_path: str) -> str:
+    wave_audio_file = wave.open(phrase_wav_path, "rb")
+
+    offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
+
+    offline_recognizer.SetWords(True)
+
+    data = wave_audio_file.readframes(wave_audio_file.getnframes())
+
+        # print(wave_audio_file.getcomptype())
+        # print(wave_audio_file.getnchannels())
+        # print(wave_audio_file.getframerate())
+
+    offline_recognizer.AcceptWaveform(data)
+    # recognized_data = json.loads(offline_recognizer.Result())
+    recognized_data = offline_recognizer.Result()
+    recognized_data=json.loads(recognized_data)
+    recognized_data=recognized_data['text']
+
+    return recognized_data if len(recognized_data)>0 else "Veuillez répéter"
+
 def setup_assistant_voice():
     voices=ttsEngine.getProperty('voices')
     if assistant.speech_language=='fr':
         ttsEngine.setProperty("voice","french")
-        ttsEngine.setProperty("rate", 100)
+
 
 def play_voice_assistant_sppech(text_to_speech):
     ttsEngine.say(str(text_to_speech))
@@ -39,7 +63,8 @@ def record_recognize_audio(*args:tuple):
             print("Reconnaissance start..")
             recognized_data=recognizer.recognize_google(audio,language='fr')
         except speech_recognition.UnknownValueError:
-            pass
+
+            recognized_data ="Veuillez repétez "
         except speech_recognition.RequestError:
             print("Essaie de la reconnaissance hors ligne")
             recognized_data=use_offline_recognition()
@@ -47,22 +72,20 @@ def record_recognize_audio(*args:tuple):
         return recognized_data
 
 def use_offline_recognition():
-    recognized_data=''
-    if not os.path.exists("models/vosk-model-small-fr-0.22"):
-        print("Veuillez télécharger un model")
-        exit(1)
-    else:
-        wave_audio_file=wave.open("microphone-results.wav","rb")
-        model=Model("models/vosk-model-small-fr-0.22")
-        offline_recognizer=KaldiRecognizer(model,wave_audio_file.getframerate())
-        data=wave_audio_file.readframes(wave_audio_file.getnframes())
-        if len(data)>0:
-            if offline_recognizer.AcceptWaveform(data):
-                recognized_data=offline_recognizer.Result()
-        recognized_data=json.loads(recognized_data)
-        recognized_data=recognized_data['text']
+    recognizer_data=""
 
-    return recognized_data
+    model = Model("models/vosk-model-small-fr-0.22")
+        # wavname = "/content/drive/MyDrive/Colab Notebooks/demo_Mono_S16_LE_44100.wav"
+    wavname = "microphone-results.wav"
+
+    out = recognize(model, wavname)
+
+    recognizer_data=out
+    print(recognizer_data)
+
+    return recognizer_data
+
+
 
 
 
@@ -81,8 +104,10 @@ if __name__=="__main__":
 
     while True:
         voice_input=record_recognize_audio()
-        #os.remove("microphone-results.wav")
+
         print(voice_input)
+
+
 
 
 
